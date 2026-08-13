1621d51023ebba5c3873aec931707189001f587e
MD Shish <134275347+MDShish@users.noreply.github.com>
2026-08-13 09:45:24 +0530

    Add: python-list-find - submission-3

---
+++ Python For Beginners/python-list-find/submission-3.py
@@ -0,0 +1,29 @@
+from typing import List # this is used to add type hints for List type
+
+def find_index(nums: List[int], target: int) -> int:
+    # i = 0
+
+    # for num in nums:
+    #     if num == target:
+    #         return i
+    #     i += 1
+
+    # return -1
+   # # for i, n in enumerate(nums):
+    #     if n == target:
+    #         return i
+    i = 0
+
+    while i < len(nums):
+        if nums[i] == target:
+            return i
+        i += 1
+
+    return -1
+
+# don't modify code below this line
+print(find_index([1, 2, 3, 4, 5], 3))
+print(find_index([1, 2, 3, 4, 5, 3], 3))
+print(find_index([1, 2, 3, 4], 1))
+print(find_index([1, 3, 4, 2], 2))
+
