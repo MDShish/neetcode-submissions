1f7fb8699cf446da1bc445b7052c5ea1b36145c9
MD Shish <134275347+MDShish@users.noreply.github.com>
2026-09-09 02:39:01 +0530

    Add: two-integer-sum - submission-16

---
+++ Data Structures & Algorithms/two-integer-sum/submission-16.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        arr = []
+        for i, num in enumerate(nums):
+            arr.append([i, num])
+        arr.sort(key = lambda x:x[1])
+        left, right = 0, len(nums) - 1
+        while left < right:
+            sum = arr[left][1] + arr[right][1]
+            if sum == target:
+                return [min(arr[left][0], arr[right][0]), max(arr[left][0], arr[right][0])]
+            elif sum < target:
+                left += 1
+            else:
+                right -= 1
+        return []
